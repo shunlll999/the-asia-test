6baf9be72b574afde481b4016902a215c7cca45b
wach <rafael5715@gmail.com>
2018-01-03 18:34:41 +0700

    Base project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/styles/css/index.css';
+import App from './routes/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
